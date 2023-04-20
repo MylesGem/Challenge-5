5af83a52b860a15c1cf61e3080c0e5ef3c274055
MylesGem <sasquatchgemignani@gmail.com>
2023-04-19 17:25:23 -0700

    Some debugging

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // todays date 
-var todayDate = moment().format('dddd,MM Do YYYY');
-('currentDay').html(todayDate);
- (document).ready (function (){
+var todayDate = moment().format("dddd, MMM Do YYYY");
+$("#currentDay").html(todayDate);
+$(document).ready (function (){
     //saveBtn click listener
     ('saveBtn').on('click', function () {
         //Jquery
         var text = $(this).siblings('.description').val();
         var time = $(this).parent().attr('id');
-        localStorage.setItem(time,text);
+        localStorage.setItem(time, text);
     })
 //time tracking func
     function timeTracker() {
         var timeNow = moment(). hour();
         //loop over time blocks
-    (".time-block").each(function () {
+    $(".time-block").each(function () {
             var blockTime = parseInt($(this).attr('id').split('hour')[1]);
             if (blockTime < timeNow) {
                 (this).removeClass('future');
@@ -33,5 +33,15 @@ var todayDate = moment().format('dddd,MM Do YYYY');
             }
         })
     }
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
     timeTracker();
 })
