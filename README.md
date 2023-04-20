af985ec3495177524a5591a0117c2ce3838b4ee5
MylesGem <sasquatchgemignani@gmail.com>
2023-04-19 18:41:36 -0700

    changes

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Weekly planner
 Started cleaning up starter code so i could understand it  better
 Added slots for all the HRS of the work day
 completed Java for schedule debugging time
+having issues with establishing a function
+i cannot get it to function maybe i am missiong something
+even used other refrence code 
+https://github.com/sylviaprabudy/work-day-scheduler
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // todays date 
 var todayDate = moment().format("dddd, MMM Do YYYY");
-$("#currentDay").html(todayDate);
-$(document).ready (function (){
+("#currentDay").html(todayDate);
+(document).ready (function (){
     //saveBtn click listener
     ('saveBtn').on('click', function () {
         //Jquery
@@ -13,7 +13,7 @@ $(document).ready (function (){
     function timeTracker() {
         var timeNow = moment(). hour();
         //loop over time blocks
-    $(".time-block").each(function () {
+    (".time-block").each(function () {
             var blockTime = parseInt($(this).attr('id').split('hour')[1]);
             if (blockTime < timeNow) {
                 (this).removeClass('future');
@@ -33,15 +33,15 @@ $(document).ready (function (){
             }
         })
     }
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    ("#hour8 .description").val(localStorage.getItem("hour8"));
+    ("#hour9 .description").val(localStorage.getItem("hour9"));
+    ("#hour10 .description").val(localStorage.getItem("hour10"));
+    ("#hour11 .description").val(localStorage.getItem("hour11"));
+    ("#hour12 .description").val(localStorage.getItem("hour12"));
+    ("#hour13 .description").val(localStorage.getItem("hour13"));
+    ("#hour14 .description").val(localStorage.getItem("hour14"));
+    ("#hour15 .description").val(localStorage.getItem("hour15"));
+    ("#hour16 .description").val(localStorage.getItem("hour16"));
+    ("#hour17 .description").val(localStorage.getItem("hour17"));
     timeTracker();
 })
